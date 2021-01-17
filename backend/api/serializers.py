8015d550e774c58f12377da1565e469774a6cf40
sudharsan004 <sudharsansriram8921@gmail.com>
2021-01-17 22:44:16 +0530

    create post serializer

---
+++ backend/api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Post
+
+class PostSerializer(serializers.Serializer):
+  
+  
+  class Meta:
+    model = Post
+    fields = ('id','title','description','user_id')
