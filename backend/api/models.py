409f727819bdf6eb40ba74021e2da0c0cca18700
sudharsan004 <sudharsansriram8921@gmail.com>
2021-01-17 21:45:27 +0530

    create post model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=100)
+  description = models.CharField(max_length=350)
+  user_id = models.IntegerField()
+  created_at = models.DateTimeField(auto_add_now=True)
