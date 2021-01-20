d284d7cae796b66a2441e02576d4d1699708c870
sudharsan004 <sudharsansriram8921@gmail.com>
2021-01-20 08:24:37 -0800

    update post component

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,19 +1,19 @@
-import {React} from 'react'
+import { React } from 'react'
 import DeleteIcon from "@material-ui/icons/Delete";
 import './Post.css'
 
 function Post(props) {
 
-    return (
-        
-      <div className="post">
+  return (
+
+    <div className="post">
       <h1>{props.title}</h1>
       <p>{props.description}</p>
       <button >
         <DeleteIcon />
       </button>
     </div>
-    )
+  )
 }
 
 export default Post
