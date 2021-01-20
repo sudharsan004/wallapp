81ff8cffcadc7ad8ca3a4e2ff49a606bc07812d2
sudharsan004 <sudharsansriram8921@gmail.com>
2021-01-20 08:23:53 -0800

    update post component

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -6,11 +6,10 @@ function Post(props) {
 
     return (
         
-        <div className="post">
+      <div className="post">
       <h1>{props.title}</h1>
       <p>{props.description}</p>
       <button >
-          {console.log(props)}
         <DeleteIcon />
       </button>
     </div>
