ce02aecafb43c14c203f6f1e99639cdef01df247
sudharsan004 <sudharsansriram8921@gmail.com>
2021-01-21 19:14:32 -0800

    conditional render delete component

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -3,16 +3,20 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import './css/Post.css'
 
 function Post(props) {
-  let {id,title,description,user_name} = props
+  let { id, title, description, user_name } = props
   return (
 
     <div className="post">
       <h1>{title}</h1>
       <p>{description}</p>
       <span className='small'>author: {user_name}</span>
+      {
+      (window.localStorage.username=== user_name) ? (
       <button >
         <DeleteIcon />
-      </button>
+      </button>) : ""
+      }
+
     </div>
   )
 }
