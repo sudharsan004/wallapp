e6bcba5af2f75169dcf235bd04ac620420d01146
sudharsan004 <sudharsansriram8921@gmail.com>
2021-01-21 16:54:15 -0800

    update Header Nav

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import BrushIcon from '@material-ui/icons/BrushTwoTone';
 import './css/Header.css'
+import { Link } from 'react-router-dom';
 function Header() {
-    return (
-        <header>
+  return (
+    <header>
       <h1>
+        
         <BrushIcon />
         Wall App
       </h1>
-      <ul>
-  <li><a href="default.asp">Home</a></li>
-  <li><a href="news.asp">News</a></li>
-  <li><a href="contact.asp">Contact</a></li>
-  <li><a href="about.asp">About</a></li>
-</ul> 
+      <nav> 
+        <ul>
+        <li><Link to="login">Login</Link></li>
+        <li><Link to="register">Register</Link></li>
+        </ul>
+      </nav>
     </header>
     )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import {Route, Link, Switch, BrowserRouter as Router} from "react-router-dom";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Login from './components/AuthComponents/Login';
 import Register from './components/AuthComponents/Register';
 
 
 const routing = (
   <Router>
-    <Route exact path="/" component={App}/>
-    <Route path="/login" component={Login}/>
-    <Route path="/register" component={Register}/>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Switch>
   </Router>
 )
 
 ReactDOM.render(
-    routing
-  ,document.getElementById('root')
+  routing
+  , document.getElementById('root')
 );
